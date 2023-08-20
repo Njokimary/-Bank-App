6ff8caadaccaf0772c5d05cd2453d6ad66b61a78
MARY NJOKI <marynjoki738@gmail.com>
2023-08-20 22:30:38 +0300

    created a post function and updated date in the form

--- src/AddTransactionForm.js
+++ src/AddTransactionForm.js
@@ -5,24 +5,57 @@ function AddTransactionForm({ addTransaction }) {
   const [description, setDescription] = useState('');
   const [category, setCategory] = useState('');
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
     const newTransaction = {
       amount,
       description,
       category,
+      date: new Date().toISOString(),
     };
-    addTransaction(newTransaction);
-    setAmount('');
-    setDescription('');
-    setCategory('');
+
+    try {
+      const response = await fetch('http://localhost:8000/transactions', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newTransaction),
+      });
+
+      if (response.ok) {
+        addTransaction(newTransaction);
+        setAmount('');
+        setDescription('');
+        setCategory('');
+      } else {
+        console.error('Failed to add transaction');
+      }
+    } catch (error) {
+      console.error('Error adding transaction:', error);
+    }
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <input type='text' value={amount} onChange={(e) => setAmount(e.target.value)} placeholder='Amount' />
-      <input type='text' value={description} onChange={(e) => setDescription(e.target.value)} placeholder='Description' />
-      <input type='text' value={category} onChange={(e) => setCategory(e.target.value)} placeholder='Category' />
+      <input
+        type='text'
+        value={amount}
+        onChange={(e) => setAmount(e.target.value)}
+        placeholder='Amount'
+      />
+      <input
+        type='text'
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+        placeholder='Description'
+      />
+      <input
+        type='text'
+        value={category}
+        onChange={(e) => setCategory(e.target.value)}
+        placeholder='Category'
+      />
       <button type='submit'>Add Transaction</button>
     </form>
   );
