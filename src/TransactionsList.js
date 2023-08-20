dcde48af1c953930eb549223571e1c46ae60743a
MARY NJOKI <marynjoki738@gmail.com>
2023-08-20 11:54:17 +0300

    created a TransactionList file with the transactions table

---
+++ src/TransactionsList.js
@@ -0,0 +1,25 @@
+import react from 'react';
+
+function TransactionList({ transactions }) {
+    return(
+        <table className='transactions-list'>
+        <tbody>
+        <tr className='table-header'>
+          <th>
+            <h3 className='header-cell'>Date</h3>
+          </th>
+          <th>
+            <h3 className='header-cell'>Description</h3>
+          </th>
+          <th>
+            <h3 className='header-cell'>Category</h3>
+          </th>
+          <th>
+            <h3 className='header-cell'>Amount</h3>
+          </th>
+        </tr>
+        </tbody>
+        </table>
+    )
+}
+export default TransactionList
