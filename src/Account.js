d47ac823f2ee33ac232e01818cc9da4e4b00b23a
MARY NJOKI <marynjoki738@gmail.com>
2023-08-21 13:56:42 +0300

    created delete function

--- src/Account.js
+++ src/Account.js
@@ -31,13 +31,31 @@ function AccountContainer() {
     setTransactions([...transactions, newTransaction]);
   };
 
+  const deleteTransaction = transactionId => {
+    // Perform API call or other logic to delete the transaction
+    const updatedTransactions = transactions.filter(
+      transaction => transaction.id !== transactionId
+    );
+    setTransactions(updatedTransactions);
+
+    // Update filteredTransactions with updatedTransactions based on searchTerm
+    setFilteredTransactions(
+      updatedTransactions.filter(transaction =>
+        transaction.description.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+    );
+  };
+
   return (
     <div className="container">
       <div className="header">
         <h1>Transaction Tracker</h1>
       </div>
       <Search handleSearch={handleSearch} />
-      <TransactionsList transactions={filteredTransactions} />
+      <TransactionsList
+        transactions={filteredTransactions}
+        onDeleteTransaction={deleteTransaction}
+      />
       <AddTransactionForm addTransaction={addTransaction} />
     </div>
   );
