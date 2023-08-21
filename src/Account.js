7085ddda45eff1d08df65f528ddf8f192ff25885
MARY NJOKI <marynjoki738@gmail.com>
2023-08-21 12:49:24 +0300

    created the filter function by description

--- src/Account.js
+++ src/Account.js
@@ -7,28 +7,32 @@ import './App.css';
 function AccountContainer() {
   const [searchTerm, setSearchTerm] = useState('');
   const [transactions, setTransactions] = useState([]);
+  const [filteredTransactions, setFilteredTransactions] = useState([]);
 
   useEffect(() => {
     fetch('http://localhost:8000/transactions')
       .then(response => response.json())
-      .then(data => setTransactions(data))
+      .then(data => {
+        setTransactions(data);
+        setFilteredTransactions(
+          data.filter(transaction =>
+            transaction.description.toLowerCase().includes(searchTerm.toLowerCase())
+          )
+        );
+      })
       .catch(error => console.log(error));
-  }, []);
+  }, [searchTerm]);
 
-  const handleSearch = (term) => {
+  const handleSearch = term => {
     setSearchTerm(term);
   };
 
-  const filteredTransactions = transactions.filter((transaction) =>
-    transaction.description.toLowerCase().includes(searchTerm.toLowerCase())
-  );
-
-  const addTransaction = (newTransaction) => {
+  const addTransaction = newTransaction => {
     setTransactions([...transactions, newTransaction]);
   };
 
   return (
-    <div  className="container">
+    <div className="container">
       <div className="header">
         <h1>Transaction Tracker</h1>
       </div>
